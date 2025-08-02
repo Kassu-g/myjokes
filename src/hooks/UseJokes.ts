26872f70f64be1baecf61fad1017faca6f781601
KGröhn <64731263+Kassu-g@users.noreply.github.com>
2025-08-02 13:02:41 +0300

    fix: prevent lost jokes when saving rapidly

--- src/hooks/UseJokes.ts
+++ src/hooks/UseJokes.ts
@@ -11,11 +11,15 @@ export const useJokes = () => {
   const [savedJokes, setSavedJokes] = useState<IJoke[]>([]);
 
   const addJoke = (joke: IJoke): boolean => {
-    if (savedJokes.some((j) => j.id === joke.id)) {
-      return false;
-    }
-    setSavedJokes([...savedJokes, joke]);
-    return true;
+    let added = false;
+    setSavedJokes((prevJokes) => {
+      if (prevJokes.some((j) => j.id === joke.id)) {
+        return prevJokes;
+      }
+      added = true;
+      return [...prevJokes, joke];
+    });
+    return added;
   };
 
   return { savedJokes, addJoke };
